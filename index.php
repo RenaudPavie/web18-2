d3b873c8c0fdf3abdf410a7310c551905b2cb21a
Frédéric NOËL <contact@fredericnoel.com>
2019-05-06 16:08:08 +0200

    Includesion fichiers

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-$resultat = '<ul>';
-for ($i = -12328 ; $i <= 23456 ; $i += 7) {
-    if ($i % 5 == 0) {
-        $resultat .= '<li>' . $i . '</li>';
-    }
-}
-
-$resultat .= '</ul>';
-
-echo $resultat;
+require_once './includes/header.php';
+require_once './includes/content.php';
+require_once './includes/footer.php';
