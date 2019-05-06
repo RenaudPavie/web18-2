fb08562470e6a39957d14c6fb266a1f46750079a
Frédéric NOËL <contact@fredericnoel.com>
2019-05-06 14:58:00 +0200

    Truc amusant avec une boucle for

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
 $resultat = '<ul>';
-for ($i = 1 ; $i <= 100 ; $i++) {
-    $resultat .= '<li>' . $i . '</li>';
+for ($i = -12328 ; $i <= 23456 ; $i += 7) {
+    if ($i % 5 == 0) {
+        $resultat .= '<li>' . $i . '</li>';
+    }
 }
 
 $resultat .= '</ul>';
