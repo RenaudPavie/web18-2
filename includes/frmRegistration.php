1c6c3ad98acb9ae0723450a930710f2f9cf81db7
Frédéric NOËL <contact@fredericnoel.com>
2019-06-03 14:06:12 +0200

    Ajout attribut value

--- includes/frmRegistration.php
+++ includes/frmRegistration.php
@@ -1,15 +1,20 @@
+<?php
+$nom = isset($nom) ? $nom : "";
+$prenom = isset($prenom) ? $prenom : "";
+$email = isset($email) ? $email : "";
+?>
 <form method="post" action="index.php?page=registration">
     <div>
         <label for="nom">Nom&nbsp;: </label>
-        <input name="nom" type="text" />
+        <input name="nom" type="text" value="<?=$nom?>" />
     </div>
     <div>
         <label for="prenom">Prénom&nbsp;: </label>
-        <input name="prenom" type="text" />
+        <input name="prenom" type="text" value="<?=$prenom?>" />
     </div>
     <div>
         <label for="email">e-mail&nbsp;: </label>
-        <input name="email" type="mail" />
+        <input name="email" type="mail" value="<?=$email?>"/>
     </div>
     <div>
         <label for="mdp">Mot de passe&nbsp;: </label>
