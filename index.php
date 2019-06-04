d25bac150d27f68d016cf3781e95739bc484213a
Frédéric NOËL <contact@fredericnoel.com>
2019-06-04 16:39:04 +0200

    Ajout fuseau horaire. Ajout démarrage session.

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+session_start();
 
+date_default_timezone_set('Europe/Paris');
+
+require_once './includes/pdo.php';
 require_once './functions/getLang.php';
 require_once './includes/head.php';
 require_once './includes/header.php';
