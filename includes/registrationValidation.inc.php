0b41ae6f5aa9a93efda166502c50fdf06b29afce
Frédéric NOËL <contact@fredericnoel.com>
2019-06-04 13:56:31 +0200

    WIP - Requête avec mail et token - Vérification du nombre de résultats - Vérification horodatage

--- includes/registrationValidation.inc.php
+++ includes/registrationValidation.inc.php
@@ -3,5 +3,33 @@
 $mail = isset($_GET['mail']) ? $_GET['mail'] : "";
 $token = isset($_GET['token']) ? $_GET['token'] : "";
 
-echo "<p>Mail : $mail</p>";
-echo "<p>Token : $token</p>";
+$sql = "SELECT * FROM t_users
+        WHERE usemail='$mail'
+        AND usetoken='$token'";
+
+$result = $pdo->query($sql);
+
+if ($result->rowCount() == 0) {
+    echo "Dis donc Michel, tu fais quoi avec l'URL ?";
+}
+
+
+else {
+    $truc = $result->fetchObject();
+    var_dump(strtotime($truc->usedatetime));
+    echo strtotime('now');
+
+    $timeRegistration = strtotime($truc->usedatetime);
+    $timeNow = strtotime('now');
+
+    $delta = $timeNow - $timeRegistration;
+
+    if ($delta <= 3600) {
+        echo "Yes";
+    }
+
+    else {
+        echo "Trop tard Michel";
+    }
+}
+
