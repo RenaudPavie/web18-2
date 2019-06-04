e29b8ec50531b1d34e46fd0e2b4133eeba8eea3a
Frédéric NOËL <contact@fredericnoel.com>
2019-06-04 10:03:51 +0200

    WIP : test récupération des paramètres d'URL mail et token

--- includes/registrationValidation.inc.php
+++ includes/registrationValidation.inc.php
@@ -1,3 +1,7 @@
 <?php
 
-echo "Michel";
+$mail = isset($_GET['mail']) ? $_GET['mail'] : "";
+$token = isset($_GET['token']) ? $_GET['token'] : "";
+
+echo "<p>Mail : $mail</p>";
+echo "<p>Token : $token</p>";
