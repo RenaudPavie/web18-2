5a11c06d07d9a20cf6619aa9bb66526118882abe
Frédéric NOËL <contact@fredericnoel.com>
2019-06-06 11:14:06 +0200

    Ajout spl_autoload_registre() pour chargement automatique des classes.

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 session_start();
 
 date_default_timezone_set('Europe/Paris');
+require_once './functions/classAutoLoader.php';
+spl_autoload_register('classAutoLoader');
 
 require_once './includes/pdo.php';
 require_once './functions/getLang.php';
+require_once './functions/secureForm.php';
 require_once './includes/head.php';
 require_once './includes/header.php';
 require_once './includes/content.php';
