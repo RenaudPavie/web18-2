5859fa1f5f7d07a5db45af52fa963f67cd8eae41
Frédéric NOËL <contact@fredericnoel.com>
2019-06-06 11:10:02 +0200

    Ajout lien vers la page Game en vérifiant que l'utilisateur est connecté

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,21 @@
+<?php
+if (isset($_SESSION['login']) && $_SESSION['login'] == 1)
+    $logValue = "logout";
+else
+    $logValue = "login";
+?>
 <header>
     <nav>
         <ul>
             <li><a href="index.php?page=home" title="Page d'accueil">Accueil</a></li>
             <li><a href="index.php?page=news" title="Actualités">Actualités</a></li>
-            <li><a href="index.php?page=login" title="Login">Login</a></li>
+            <li><a href="index.php?page=<?=$logValue?>" title="<?=ucfirst($logValue)?>"><?=ucfirst($logValue)?></a></li>
             <li><a href="index.php?page=registration" title="Inscription">Inscription</a></li>
+            <?php
+                // Pour afficher le lien vers la page 'Game', on vérifie que l'utilisateir est connecté
+                if (isset($_SESSION['login']) && $_SESSION['login'] == 1)
+                     echo "<li><a href=\"index.php?page=game\" title=\"Game\">Game</a></li>";
+            ?>
         </ul>
     </nav>
 </header>
